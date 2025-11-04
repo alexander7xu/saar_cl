a4f358588c09214317316ebd869b934ab1e505c1
alexander7xu <alexander7xu@gmail.com>
2025-11-04 03:09:34 +0100

    Assigment 1

---
+++ 1_n‐gram_models/README.md
@@ -0,0 +1,40 @@
+# Saar CoLi Assignment 1: n‐gram models
+
+## Author
+
+Juangui Xu
+
+## Directory Structure
+
+```
+    1_1_Zipfs_Law.ipynb                 -- Solution to Q1
+    1_2_Random_Text_Generation.ipynb    -- Solution to Q2
+    1_3_Statistical_Dependence.ipynb    -- Solution to Q3
+    ngram.py                            -- Official package of N-Gram
+    README.md                           -- This README file
+    utils1.py                           -- Functions shared by the solutions
+```
+
+## Environments
+
+### Device
+
+- CPU: Snapdragon (TM) 8cx Gen 3 @ 2.69 GHz
+- RAM: 16 GB
+- OS: Windows 11 Pro ARM 25H2
+
+### Packages
+
+```
+Python==3.12.10
+datasets==4.2.0
+ipykernel==7.0.1
+matplotlib==3.10.7
+nltk==3.9.2
+numpy==2.3.4
+requests==2.32.5
+```
+
+## Runtime
+
+The estimated runtime depends on the device. Running the script on my personal device described above takes no more than 30 secs for each whole ipynb.
---
+++ 1_n‐gram_models/utils1.py
@@ -0,0 +1,72 @@
+import datasets
+import requests
+
+from typing import Hashable, Iterable, OrderedDict
+from pathlib import Path
+import string
+
+
+def load_dataset(
+    name: str, cache_dir: str | Path | None = None
+) -> str | datasets.Dataset:
+    """
+    Load different kind (e.g. from URL on HuggingFace) of dataset by name
+    - Arguments:
+        - name: name of dataset
+        - cache_root: path to save. Could be None, then:
+            - HF datasets would be save to their default cache folder.
+            - URL datasets would be downloaded without saving in local.
+    """
+
+    NAME_TO_URL = {
+        "King James Bible": "https://raw.githubusercontent.com/coli-saar/cl/main/a1/kingjamesbible_tokenized.txt",
+        "The Jungle Book": "https://raw.githubusercontent.com/coli-saar/cl/main/a1/junglebook.txt",
+    }
+    NAME_TO_HF = {"SETIMES": ["community-datasets/setimes", "bg-tr"]}
+
+    if name in NAME_TO_URL:
+        path = cache_dir and (Path(cache_dir) / f"{name}.txt")
+        if path is not None and path.exists():
+            with open(path, encoding="utf-8") as file:
+                return file.read()
+        content = requests.get(NAME_TO_URL[name]).content.decode("utf-8")
+        if path is not None:
+            path.parent.mkdir(parents=True, exist_ok=True)
+            with open(path, "w", encoding="utf-8") as file:
+                file.write(content)
+        return content
+    elif name in NAME_TO_HF:
+        return datasets.load_dataset(*NAME_TO_HF[name], cache_dir=cache_dir)
+    else:
+        raise NotImplementedError(name)
+
+
+def simple_tokenize(text: str, remove_symbols: bool = False) -> list[str]:
+    """
+    - Actions:
+        - Convert to lowercase
+        - Replace invisible chars with whitespace
+        - Splitting at whitespace(s)
+        - (Optional) remove symbols
+    """
+    set_to_remove = set(string.whitespace)
+    if remove_symbols:
+        set_to_remove |= set(string.punctuation)
+    text = "".join([" " if c in set_to_remove else c for c in text])
+    result = text.lower().split(" ")
+    result = [x for x in result if len(x) > 0]
+    return result
+
+
+def counter(items: Iterable[Hashable]) -> OrderedDict[Hashable, int]:
+    """
+    - Return:
+        - unique items
+        - corresponding counts
+    """
+    obj_to_cnt: dict[str, int] = dict()
+    for obj in items:
+        obj_to_cnt[obj] = obj_to_cnt.get(obj, 0) + 1
+
+    obj_cnt = sorted(obj_to_cnt.items(), key=lambda x: -x[-1])
+    return OrderedDict(obj_cnt)
